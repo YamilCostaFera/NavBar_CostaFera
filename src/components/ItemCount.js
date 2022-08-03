fe9cdb7683260ae7df784785c524668760d5ff11
YamilCostaFera <yacf87@gmail.com>
2022-08-03 20:00:20 -0300

    Desafio Contador con boton

---
+++ src/components/ItemCount.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+    function ItemCount(props){
+    console.log(props.stock);    
+
+    const [cant, setClicks] = React.useState(1);
+
+    const handleIncrement= () => {
+        cant===props.stock ? alert ("no hay mas stock") :  setClicks(cant + 1);
+    }    
+
+    const handleDecrement= () => {
+        cant===props.initial ? setClicks (cant) : setClicks(cant - 1);
+    }    
+
+    const onAdd = () => {
+        alert ("Añadido al carrito");
+    }
+
+    return(
+        <>
+        <div class='container-fluid'>
+            <h1>Cantidad</h1>
+            <h3> {cant} </h3>
+            <div class="btn-group mb-2 col-2" role="group" aria-label="Basic outlined example">    
+                <button onClick={handleDecrement } type="button" class="btn btn-outline-primary" > - </button>
+                <button onClick={ handleIncrement } type="button" class="btn btn-outline-primary" > + </button>
+            </div>
+            <div>
+                <button onClick={onAdd} class='mt-4 col-4 btn btn-outline-primary' > Añadir al carrito </button>
+            </div>
+        </div>
+        
+        </>
+    )    
+
+}
+
+export default ItemCount;
+
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 function ItemListContainer(props) {
     return (
-        <div className="container-fluid col-5 card mt-4">
-            <h1 className="card_style">Torta: {props.greeting}</h1>
+        <div>
+            <div className="container-fluid col-5 card mt-4">
+                <h1 className="card_style">Torta: {props.greeting}</h1>
+            </div>
+            <ItemCount initial={1} stock={3}/>
         </div>
     )
 }
